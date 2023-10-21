b08f8389c86a58753d8de35a2bbe32f947c342aa
IvMysl <IvMysl_2009@gmail.com>
2023-10-21 16:46:49 +0300

    adaptive seconds

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menuList = document.querySelector(".nav__list");
+
+menuBtn.addEventListener("click", () => {
+  menuList.classList.toggle("nav__active");
+});
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("menu__active");
+});
+
 function startTimer() {
   let minutes = 1;
   let seconds = 10;
